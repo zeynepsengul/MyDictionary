73fc29264eb6a59ef4b9550a3f7c7c920dea665c
Zeynep Şengül <zeynepsengul1650@gmail.com>
2021-08-21 14:34:15 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+   
+    class MyDictionary<T1, T>
+    {
+        T1[] keys;
+        T[] items;
+        public MyDictionary()
+        {
+            keys = new T1[0];
+            items = new T[0];
+        }
+        public void Add(T1 key, T item)
+        {
+            T1[] tempKeys = keys;
+            T[] tempArray = items;
+
+            items = new T[items.Length + 1];
+            keys= new T1[keys.Length + 1];
+
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+                keys[i] =  tempKeys[i];
+            }
+            items[items.Length - 1] = item;
+            keys[keys.Length - 1] = key;
+        }
+    }
+}
+ 
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> isimler = new MyDictionary<int, string>();
+            isimler.Add(12, "Zeynep");
+        }
+    }
+}
